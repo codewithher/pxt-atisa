f9251d8649ea6990957e5aaec5da8389d8158176
Ryan <30853424+totally-not-frito-lays@users.noreply.github.com>
2025-05-30 17:46:41 -0700

    refactored test structure to be feature focused

--- test/features/welcome/e2e.test.js
+++ test/features/welcome/e2e.test.js
@@ -2,11 +2,9 @@ const { Builder, By, until } = require('selenium-webdriver');
 const chai = require('chai');
 const expect = chai.expect;
 
-// Set default timeouts
 const DEFAULT_TIMEOUT = 10000; // 10 seconds
 const SHORT_TIMEOUT = 5000; // 5 seconds
 
-// Set up the driver
 let driver;
 
 before(async () => {
@@ -21,12 +19,11 @@ after(async () => {
     }
 });
 
-describe('Webdriver Tests', function() {
-    this.timeout(DEFAULT_TIMEOUT); // Increase timeout to 10 seconds
+describe('Welcome Page Renders DOM', function() {
+    this.timeout(DEFAULT_TIMEOUT);
     
-    // Reset browser state before each test
     beforeEach(async function() {
-        this.timeout(DEFAULT_TIMEOUT); // Increase timeout for each test
+        this.timeout(DEFAULT_TIMEOUT);
         await driver.get('http://localhost:3232');
         await driver.wait(until.elementLocated(By.css('body')), DEFAULT_TIMEOUT);
     });
@@ -37,11 +34,9 @@ describe('Webdriver Tests', function() {
     });
 
     it('should find and verify the New Project button', async () => {
-        // Wait for the element with the specific title
         const newProjectElement = await driver.wait(until.elementLocated(By.xpath("//div[@title='Creates a new empty project']")), SHORT_TIMEOUT);
         expect(newProjectElement).to.be.ok;
         
-        // Check if the element is clickable
         await newProjectElement.click();
         expect(newProjectElement).to.be.ok;
     });
--- test/features/welcome/unit.test.js
+++ test/features/welcome/unit.test.js
@@ -1,4 +1,6 @@
 const { expect } = require('chai');
+
+// Placeholder test
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
